c61c115b8db6199416b5c7d34d6710445b17d1f3
krikaz <pascal.bou@protonmail.com>
2019-07-15 13:45:18 +0200

    get all and post new

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-
 const db = require('./data/dbConfig.js');
-
 const server = express();
 
+function getAllAccounts() {
+	return db('accounts');
+}
+
+function createNewAccount() {
+  return db('users').insert({ name, budget });
+}
+
 server.use(express.json());
+
+server.get('/accounts', async (req, res) => {
+	const result = await getAllAccounts();
+	res.json(result);
+});
+
+app.post('/users', async (req, res) => {
+	const arrayIds = await createNewAccount(req.body);
+	res.json(arrayIds[0]);
+});
 
 module.exports = server;
