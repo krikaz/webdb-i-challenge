63b429f5d97d324610bf6be4d232b719acbfc1d6
krikaz <pascal.bou@protonmail.com>
2019-07-15 13:54:25 +0200

    added all endpoints

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ function getAllAccounts() {
 	return db('accounts');
 }
 
+function getAccountById(id) {
+	return db('accounts').where({ id });
+}
+
+function deleteAccountById(id) {
+	return db('accounts')
+		.where({ id })
+		.del();
+}
+
 function createNewAccount({ name, budget }) {
 	return db('accounts').insert({ name, budget });
+}
+
+function updateAccountById(id, { name, budget }) {
+	return db('accounts')
+		.where({ id })
+		.update({ name, budget });
 }
 
 server.use(express.json());
@@ -17,9 +33,25 @@ server.get('/accounts', async (req, res) => {
 	res.json(result);
 });
 
+server.get('/accounts/:id', async (req, res) => {
+	const result = await getAccountById(req.params.id);
+	res.json(result);
+});
+
+server.delete('/accounts/:id', async (req, res) => {
+	const result = await deleteAccountById(req.params.id);
+	res.json(result);
+});
+
 server.post('/accounts', async (req, res) => {
 	const arrayIds = await createNewAccount(req.body);
 	res.json(arrayIds[0]);
+});
+
+app.put('/accounts/:id', async (req, res) => {
+	const { name, budget } = req.body;
+	const result = await updateAccountById(req.params.id, { name, budget });
+	res.json(result);
 });
 
 module.exports = server;
